0a4c86044d327fce37893ebaf3a6b8851a8f8cf5
Elston Neil D'Souza <elstonndsouza@gmail.com>
2023-05-12 11:00:10 +0100

    Fix paths to the downloads script

--- download_all.sh
+++ download_all.sh
@@ -2,10 +2,10 @@
 
 
 ### Download MANE v1 ###
-# To run
-# bash download_mane --mane_v 0.95
+# To run e.g.
+# bash download_all.sh --mane_v 0.95
 
-mane_v=${mane_v:-0.93}
+mane_v=${mane_v:-1.0}
 
 while [ $# -gt 0 ]; do
 
@@ -21,14 +21,11 @@ done
 # Base url
 ftp_url="ftp://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/release_${mane_v}/*"
 
-# CD to script directory
-cd $(dirname "$0")
-
 # Create MANE directory if it doesn't exist
-mkdir -p ../../data/pipeline/MANE/${mane_v}
+mkdir -p data/MANE/${mane_v}
 
 # Download to directory
-wget --no-parent $ftp_url -P ../../data/pipeline/MANE/${mane_v}
+wget --no-parent $ftp_url -P 5-UTR_char/MANE/${mane_v}
 
 echo "Completed downloading MANE ${mane_v}"
 
@@ -36,21 +33,12 @@ echo "Completed downloading MANE ${mane_v}"
 # downloads constraint metrics from gnomad's public bucket
 # and not the requester pays bucket
 
-# ensure that gsutil / bgzip is installed
-
-# Currently using exomes 2.1.1
-
-# TODO : Update when the new constraint metrics will be out
-cd $(dirname "$0")
-
-cd ../../
-
 # Paths to the public data buckets
 gs_contraint_transcript="gs://gcp-public-data--gnomad/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_transcript.txt.bgz"
 
 # Download from the gnomad storage bucket
-gsutil cp gs://gcp-public-data--gnomad/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_transcript.txt.bgz data/pipeline/GNOMAD
+gsutil cp gs://gcp-public-data--gnomad/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_transcript.txt.bgz 5-UTR_char/
 
 # Unzip the files as the raw files a bgzipeed
-bgzip -d data/pipeline/GNOMAD/gnomad.v2.1.1.lof_metrics.by_transcript.txt.bgz
+bgzip -d 5-UTR_char/gnomad.v2.1.1.lof_metrics.by_transcript.txt.bgz
 
